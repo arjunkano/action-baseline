0007a01e73570670d0019f0087b245495c38174c
arjun <arjun@qualizeal.com>
2021-12-16 21:21:56 +0530

    Added more commands

--- dist/index.js
+++ dist/index.js
@@ -3831,6 +3831,10 @@ async function run() {
         let command = (`docker run --user root -v ${workspace}:/zap/wrk/:rw --network="host" ` +
            `-t ${docker_name} zap.sh -cmd -quickurl ${target} -quickout ./${htmlReportName}`);
         let command2 = (`docker run --user root -v ${workspace}:/zap/wrk/:rw --network="host" ` +
+           `-t ${docker_name} pwd`);
+        let command3 = (`docker run --user root -v ${workspace}:/zap/wrk/:rw --network="host" ` +
+           `-t ${docker_name} ls`);   
+        let command4 = (`docker run --user root -v ${workspace}:/zap/wrk/:rw --network="host" ` +
            `-t ${docker_name} cat /zap/./${htmlReportName}`);
 
         if (plugins.length !== 0) {
@@ -3840,6 +3844,8 @@ async function run() {
         try {
             await exec.exec(command);
             await exec.exec(command2);
+            await exec.exec(command3);
+            await exec.exec(command4);
         } catch (err) {
             if (err.toString().includes('exit code 3')) {
                 core.setFailed('failed to scan the target: ' + err.toString());
